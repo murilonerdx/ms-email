3da0af184794f7f56b4931789355ba5f10aa438b
Murilonerdx <mu-silva@outlook.com>
2021-07-26 15:17:54 -0300

    Criando estrutura e model

---
+++ src/test/java/com/murilonerdx/msemail/MsemailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilonerdx.msemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsemailApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
